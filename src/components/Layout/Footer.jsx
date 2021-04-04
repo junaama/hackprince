ef001fa8b4017ab3d48a863ae73f6238e09c910b
junaama <0naama0@gmail.com>
2021-04-04 08:38:14 -0400

    styled nav and footer

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -1,12 +1,17 @@
 import React from "react";
+import './layout.css';
+
 const Footer = () => {
   return (
-    <div>
-      <ul>
-          2021 HackPrinceton
-          <li>Terms & Conditions</li>
-          <li>Privacy Policy</li>
-          <li><a rel="noopener" href='https://github.com/junaama/hackprince'></a>GitHub Source</li>
+    <div className="footer">
+      <ul className="footer-ul">
+        <li>© 2021 HackPrinceton</li>
+        <li>Naama Paulemont</li>
+        <li>James Cummins</li>
+        <li>Olivia Cao</li>
+        <li>
+          <a href="https://github.com/junaama/hackprince">GitHub</a>
+        </li>
       </ul>
     </div>
   );
--- src/components/Layout/Nav.jsx
+++ src/components/Layout/Nav.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {Link} from 'react-router-dom'
+import seed from './seed2.svg'
 const Nav = () => {
     return (
-        <div>
-            <nav>
-                <ul>
+        <div className="nav-ctn">
+            <img src={seed} alt="Seedling Logo"></img>
+            <nav className="nav">
+                
+                <ul className="nav-ul">
                     <li><Link to="/">Home</Link> </li>
-                    <li><Link to="/">About</Link></li>
-                    <li><Link to="/">Sign Up</Link></li>
+                    <li><Link to="/dashboard">Dashboard</Link></li>
+                    <li><Link to="/recipe">Recipe</Link></li>
                      
                 </ul>
             </nav>

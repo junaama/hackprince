caafb4475f7f0c595d4f1f448278437a60968ba6
junaama <0naama0@gmail.com>
2021-04-04 02:15:02 -0400

    add layout

--- src/App.js
+++ src/App.js
@@ -3,17 +3,18 @@ import Home from './components/Home/Home';
 import Dashboard from './components/Dashboard/Dashboard'
 import Register from './components/UserAuth/Register'
 import Login from './components/UserAuth/Login'
+import Nav from './components/Layout/Nav'
+import Footer from './components/Layout/Footer'
 import {Route,Switch} from 'react-router'
 function App() {
   return (
     <div className="App">
-      hello world
-      Nav
+      <Nav/>
       <Switch>
         <Route exact path="/" component={Home}/>
         <Route path="/dashboard" component={Dashboard}/>
       </Switch>
-      Footer
+      <Footer/>
     </div>
   );
 }
---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+const Footer = () => {
+  return (
+    <div>
+      <ul>
+          2021 HackPrinceton
+          <li>Terms & Conditions</li>
+          <li>Privacy Policy</li>
+          <li><a rel="noopener" href='https://github.com/junaama/hackprince'></a>GitHub Source</li>
+      </ul>
+    </div>
+  );
+};
+export default Footer;
